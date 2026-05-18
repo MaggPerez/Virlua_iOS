385dc557df3837d9deb2179bb0d947c49121846e
MaggPerez <perezmag5137@gmail.com>
2026-05-18 13:27:38 -0400

    Initial Commit

---
+++ Virlua/VirluaApp.swift
@@ -0,0 +1,17 @@
+//
+//  VirluaApp.swift
+//  Virlua
+//
+//  Created by Magdaleno A Perez on 5/18/26.
+//
+
+import SwiftUI
+
+@main
+struct VirluaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ VirluaTests/VirluaTests.swift
@@ -0,0 +1,17 @@
+//
+//  VirluaTests.swift
+//  VirluaTests
+//
+//  Created by Magdaleno A Perez on 5/18/26.
+//
+
+import Testing
+@testable import Virlua
+
+struct VirluaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
